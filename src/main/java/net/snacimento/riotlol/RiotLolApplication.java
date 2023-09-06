c9c9dbe34972a77c250fc255afa614f944518f07
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-09-06 16:45:54 -0300

    Initial commit

---
+++ src/main/java/net/snacimento/riotlol/RiotLolApplication.java
@@ -0,0 +1,13 @@
+package net.snacimento.riotlol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotLolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiotLolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/snacimento/riotlol/RiotLolApplicationTests.java
@@ -0,0 +1,13 @@
+package net.snacimento.riotlol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiotLolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
